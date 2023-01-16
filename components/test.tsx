579789337cd20006404ce41abf735cf4771bd9f4
engageintellect <jc9361@gmail.com>
2023-01-16 09:03:45 -0800

    iit

--- components/test.tsx
+++ components/test.tsx
@@ -1,3 +1,15 @@
 export default function Test() {
-  return <div className="bg-red-500">hello world</div>;
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-3 gap-2 h-48 my-10">
+      <div className="bg-neutral-900 rounded-lg p-2 hover:bg-neutral-800 transition-all duration-200 flex items-center justify-center hover:scale-[105%]">
+        Hello
+      </div>
+      <div className="bg-neutral-900 rounded-lg p-2 hover:bg-neutral-800 transition-all duration-200 flex items-center justify-center hover:scale-[105%]">
+        There
+      </div>
+      <div className="bg-neutral-900 rounded-lg p-2 hover:bg-neutral-800 transition-all duration-200 flex items-center justify-center hover:scale-[105%]">
+        World
+      </div>
+    </div>
+  );
 }
